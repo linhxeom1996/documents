949cbb35852e96fa19ce32195781567a7baec348
linhnguyen <maplelabs@linhng2.local>
2022-09-29 13:26:10 +0700

    check wifi

---
+++ library_git/lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Create Lib"),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('Helo'),
+      ),
+    );
+  }
+}
