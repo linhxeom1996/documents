af332103843ff71b36dafa3ddbb04d771f0622fd
Nguyễn Đình Linh <linhnguyendeveloper@maplelabs.co>
2022-11-17 15:26:55 +0700

    lottie ex

---
+++ lottie_ex/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.network(
+            "https://assets10.lottiefiles.com/packages/lf20_CZva9peGiW.json"),
+        // child: Image.asset("assets/animations/cycle_rice.gif"),
+      ),
+    );
+  }
+}
